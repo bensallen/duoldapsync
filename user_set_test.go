e1bd74760c48b467801c28115a3760a08c3b7e0f
Ben Allen <bsallen@alcf.anl.gov>
2018-07-18 13:57:07 -0500

    Add tests for URLValues

--- user_set_test.go
+++ user_set_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/url"
 	"reflect"
 	"testing"
 
@@ -69,6 +70,72 @@ func TestUserSet_AddDuoResults(t *testing.T) {
 			tt.u.AddDuoResults(tt.args.result)
 			if !reflect.DeepEqual(tt.u, tt.wants) {
 				t.Fatalf("Mismatch between result %v and wants %v", tt.u, tt.wants)
+			}
+		})
+	}
+}
+
+func TestUser_URLValues(t *testing.T) {
+	type fields struct {
+		FullName    string
+		Email       string
+		FirstName   string
+		LastName    string
+		LDAP        bool
+		Duo         bool
+		NeedsUpdate bool
+	}
+	type args struct {
+		username string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    url.Values
+		wantErr bool
+	}{
+		{
+			name: "Username only",
+			args: args{username: "test1"},
+			want: url.Values{"username": []string{"test1"}},
+		},
+		{
+			name:    "No username",
+			args:    args{},
+			wantErr: true,
+		},
+		{
+			name:   "Username only",
+			args:   args{username: "test1"},
+			fields: fields{FullName: "Test One", Email: "test@example.com", FirstName: "Test", LastName: "One"},
+			want: url.Values{
+				"username":  []string{"test1"},
+				"realname":  []string{"Test One"},
+				"email":     []string{"test@example.com"},
+				"firstname": []string{"Test"},
+				"lastname":  []string{"One"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &User{
+				FullName:    tt.fields.FullName,
+				Email:       tt.fields.Email,
+				FirstName:   tt.fields.FirstName,
+				LastName:    tt.fields.LastName,
+				LDAP:        tt.fields.LDAP,
+				Duo:         tt.fields.Duo,
+				NeedsUpdate: tt.fields.NeedsUpdate,
+			}
+			got, err := u.URLValues(tt.args.username)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("User.URLValues() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("User.URLValues() = %v, want %v", got, tt.want)
 			}
 		})
 	}
