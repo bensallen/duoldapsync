a832051fa7b477b21eb49fddcfe0a1eb688de735
Ben Allen <bsallen@alcf.anl.gov>
2018-09-05 16:57:32 -0500

    Loop forever, with new cli arg --poll, which controls how long to sleep between iterations.

--- main.go
+++ main.go
@@ -13,11 +13,13 @@ import (
 var configPath string
 var debug bool
 var dryRun bool
+var pollTime int
 
 func init() {
 	pflag.StringVarP(&configPath, "config", "f", "config.json", "Path to configuration file")
 	pflag.BoolVarP(&debug, "debug", "d", false, "Enable debug output")
 	pflag.BoolVarP(&dryRun, "dryrun", "n", false, "Dry-run mode, don't actually create or delete users in Duo")
+	pflag.IntVarP(&pollTime, "poll", "p", 600, "Number of seconds to wait between polling LDAP and Duo for changes")
 }
 
 func main() {
@@ -36,68 +38,72 @@ func main() {
 	}
 	defer l.Close()
 
-	sr, err := enumUsers(l, conf.LDAPUserSearch)
-	if err != nil {
-		log.Printf("Error running search on LDAP: %v\n", err)
-		os.Exit(1)
-	}
-
-	if len(sr.Entries) == 0 {
-		log.Printf("LDAP search returned no results")
-		os.Exit(1)
-	} else {
-		if debug {
-			log.Printf("LDAP found %d results", len(sr.Entries))
-		}
-	}
-
-	userSet := UserSet{}
-	userSet.addLDAPEntries(sr.Entries, conf.LDAPUserSearch)
-
 	duoAPI := duoapi.NewDuoApi(conf.DuoAPI.Ikey, conf.DuoAPI.Skey, conf.DuoAPI.APIHost, "Duoldapsync", duoapi.SetTimeout(10*time.Second))
-
 	adminAPI := NewAdminAPI(*duoAPI, dryRun)
-	duoUsers, err := adminAPI.Users(url.Values{})
-	if err != nil {
-		log.Printf("Duo Users Enumeration Fail, %s", err)
-		os.Exit(1)
-	} else if duoUsers.Stat != "OK" {
-		log.Printf("Duo API returned status when attemping user enumeration: %s", duoUsers.Stat)
-		os.Exit(1)
-	}
 
-	userSet.addDuoResults(duoUsers)
+	// Loop forever sleeping pollTime seconds between iterations.
+	for {
+		sr, err := enumUsers(l, conf.LDAPUserSearch)
+		if err != nil {
+			log.Printf("Error running search on LDAP: %v\n", err)
+			os.Exit(1)
+		}
 
-	for _, user := range userSet {
-		if user.Duo == false {
+		if len(sr.Entries) == 0 {
+			log.Printf("LDAP search returned no results")
+			os.Exit(1)
+		} else {
 			if debug {
-				log.Printf("Creating Duo user: %s", user.Username)
-			}
-			err := user.duoCreate(adminAPI)
-			if err != nil {
-				log.Printf("Duo User Creation Failed, %s", err)
-				break
-			}
-			if conf.DuoAPI.SendEnrollEmail {
-				if debug {
-					log.Printf("Enrolling Duo user: %s", user.Username)
-				}
-				err := user.duoEnroll(adminAPI, conf.DuoAPI.EnrollValidSeconds)
-				if err != nil {
-					log.Printf("Duo User Enrollment Failed, %s", err)
-				}
-			}
-		} else if user.Duo == true && user.LDAP == false && conf.DuoAPI.DeleteUsers == true {
-			// Cleanup Duo Accounts
-			if debug {
-				log.Printf("Deleting Duo user: %s", user.Username)
-			}
-			resp, err := adminAPI.DeleteUser(user.DuoUserID)
-			if err != nil {
-				log.Printf("Duo User Delete Fail, %s", err)
-			} else if resp.Stat != "OK" {
-				log.Printf("Duo API returned status %d when attemping to delete user %s", resp.Code, user.Username)
+				log.Printf("LDAP found %d results", len(sr.Entries))
 			}
 		}
+
+		userSet := UserSet{}
+		userSet.addLDAPEntries(sr.Entries, conf.LDAPUserSearch)
+
+		duoUsers, err := adminAPI.Users(url.Values{})
+		if err != nil {
+			log.Printf("Duo Users Enumeration Fail, %s", err)
+			os.Exit(1)
+		} else if duoUsers.Stat != "OK" {
+			log.Printf("Duo API returned status when attemping user enumeration: %s", duoUsers.Stat)
+			os.Exit(1)
+		}
+
+		userSet.addDuoResults(duoUsers)
+
+		for _, user := range userSet {
+			if user.Duo == false {
+				if debug {
+					log.Printf("Creating Duo user: %s", user.Username)
+				}
+				err := user.duoCreate(adminAPI)
+				if err != nil {
+					log.Printf("Duo User Creation Failed, %s", err)
+					break
+				}
+				if conf.DuoAPI.SendEnrollEmail {
+					if debug {
+						log.Printf("Enrolling Duo user: %s", user.Username)
+					}
+					err := user.duoEnroll(adminAPI, conf.DuoAPI.EnrollValidSeconds)
+					if err != nil {
+						log.Printf("Duo User Enrollment Failed, %s", err)
+					}
+				}
+			} else if user.Duo == true && user.LDAP == false && conf.DuoAPI.DeleteUsers == true {
+				// Cleanup Duo Accounts
+				if debug {
+					log.Printf("Deleting Duo user: %s", user.Username)
+				}
+				resp, err := adminAPI.DeleteUser(user.DuoUserID)
+				if err != nil {
+					log.Printf("Duo User Delete Fail, %s", err)
+				} else if resp.Stat != "OK" {
+					log.Printf("Duo API returned status %d when attemping to delete user %s", resp.Code, user.Username)
+				}
+			}
+		}
+		time.Sleep(time.Second * time.Duration(pollTime))
 	}
 }
